ace2ad452cbfbcb8cf2e8b8dfadb47ea54b67a8d
Coder <Coder@DESKTOP-JT59BER>
2024-02-13 00:03:34 +0300

    Algoritma

---
+++ AlgoritmaSorusu/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*Algoritma
+Ekrandan bir string bir de sayı alan (aralarında virgül ile), ilgili string ifade içerisinden verilen indexteki karakteri çıkartıp ekrana yazdıran console uygulasını yazınız.
+
+Örnek: Input: Algoritma,3 Algoritma,5 Algoritma,22 Algoritma,0
+
+Output: Algritma Algortma Algoritma lgoritma*/
+
+Console.Write("Metn giriniz: ");
+var text=Console.ReadLine();
+
+var result = charRemove(text);
+Console.WriteLine($"Düzenlenmiş metin: {result}" );
+
+static string charRemove(string x)
+{
+    
+    var text=x.Split(',');
+    
+    var value = text[0];
+    var key = int.Parse((text[1]));
+
+    value= value.Remove(key, 1);
+
+
+
+    return value;
+};
