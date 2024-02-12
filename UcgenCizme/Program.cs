a76199ec65c281638fc353fd4b97555138c7c1d7
Coder <Coder@DESKTOP-JT59BER>
2024-02-12 19:46:47 +0300

    TriangleDraw

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+TriangleDraw(4);
+
+
+
+
+void TriangleDraw(int x)
+{
+    const string star = "*";
+    const string space = " ";
+
+    //      *
+    //     ***
+    //    *****
+    //   *******
+   
+
+    for (int i = 1; i <= x; i++)
+    {
+        
+        for (int j = i; j <= x; j++)
+        {
+            Console.Write(space);
+        }
+
+        for (int y = 1; y < i*2; y++)
+        {
+            Console.Write(star);
+        }
+        Console.WriteLine();
+          
+
+        
+
+    }
+
+    
+
+}
