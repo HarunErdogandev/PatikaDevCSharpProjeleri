9cd3065b25d0f9e1b500098b1372768eebf2abde
Coder <Coder@DESKTOP-JT59BER>
2024-02-13 00:58:58 +0300

    reverse

---
+++ CharacterReversalPrinting/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Linq;
+
+var input = "Merhaba Hello Question";
+
+var result = ReverseInput(input);
+
+Console.WriteLine(result);
+
+static string ReverseInput(string input)
+{
+    var words = input.Split(" ");
+    input = " ";
+    for (int i = 0; i < words.Length; i++)
+    {
+        var word = words[i].Reverse();
+        foreach (var item in word)
+        {
+            if (char.IsUpper(item))
+                input = input + item +" ";
+            else
+             input =input+item;
+        }
+        
+    }
+
+    
+
+    return input;
+}
