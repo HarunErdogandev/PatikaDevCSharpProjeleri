e0ccec5d504d76ca15e97d74ea2d06bef4c7e502
Coder <Coder@DESKTOP-VD9OLBT>
2024-02-14 01:03:10 +0300

    rererd

---
+++ CharacterReplacement/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var input = "Merhaba Hello Algoritma X";
+
+
+
+
+var words = input.Split(' ');
+
+var sentece = "";
+
+foreach (var word in words)
+{
+    sentece += " " + ReplaceChar(word);
+
+}
+Console.WriteLine(sentece);
+
+
+
+
+
+
+
+static string ReplaceChar(string input)
+{
+    
+
+    char[] charArray = input.ToCharArray();
+
+    charArray[0] = input[input.Length - 1];
+
+    charArray[input.Length - 1] = input[0];
+
+    return new string(charArray);
+}
