ca0951ce4910f404511f286796aa37f9be83e41b
Coder <Coder@DESKTOP-VD9OLBT>
2024-02-13 23:03:09 +0300

    Git

---
+++ AbsoluteSquare/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Console.WriteLine("Lütfen sayıları aralarında boşluk bırakarak girin:");
+string input = Console.ReadLine();
+
+string[] numbers = input.Split(' ');
+int sumOfDifferences = 0;
+int sumOfSquaredDifferences = 0;
+
+foreach (string number in numbers)
+{
+    int n = int.Parse(number);
+    int difference = Math.Abs(n - 67);
+
+    if (n < 67)
+    {
+        sumOfDifferences += difference;
+    }
+    else
+    {
+        sumOfSquaredDifferences += difference * difference;
+    }
+}
+
+if (sumOfDifferences > 0)
+{
+    Console.WriteLine("Output: " + sumOfDifferences);
+}
+else
+{
+    Console.WriteLine("Output: " + sumOfSquaredDifferences);
+}
